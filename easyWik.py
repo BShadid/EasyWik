4e0b5b14068df2b8043734d0af52630d2ed547e7
Benjamin Joseph Shadid <bshadid@student00.cse.nd.edu>
2017-04-01 02:40:03 -0400

    Exception handling added to easyWik.py

--- easyWik.py
+++ easyWik.py
@@ -2,34 +2,67 @@
 
 # This file is the main driver file and the one that interacts with the wikipedia API endpoints.
 
+from contextlib import contextmanager
 from commonwords import exclude
 import wikipedia
 import re
 import json
 import unicodedata
 import string
+import sys
+import os
+import string
 
 # print wikipedia.summary("Treap")
+'''
+@contextmanager
+def suppress_error():
+	with open("trash.log", "w") as devnull:
+		orig_stderr = sys.stderr
+		sys.stderr = devnull
+		try:
+			devnull.write( #ERROR
+		finally:
+			sys.stderr = orig_stderror'''
 
 
-def prep(query):
-	response = wikipedia.WikipediaPage(query)
+def prep():
+	while (True):	
+		query = raw_input("What would you like to search?  ")
+		try:
+			response = wikipedia.WikipediaPage(query)
+			break
+		except wikipedia.exceptions.PageError:
+			print "That Wikipedia article does not exist, and no valid suggestions could be made."
+		except wikipedia.exceptions.DisambiguationError:
+			print "Multiple articles matched this name. Try to be more specific."
+			print "Here is a list of possibly related articles:"
+			print " "
+			dummy = wikipedia.WikipediaPage(query) #Automatically breaks the script, no way to capture stderr
+
+
 	links_master = [ str(i.encode('ascii','ignore')) for i in response.links ]
 	s = response.content
 	section_names = [i.strip() for i in re.findall("==([^=]+)==", s.encode('ascii','ignore'))]
 	sections_content = []
 
+	sections_content.append(("Summary", str(unicodedata.normalize("NFKD",wikipedia.summary(query)).encode('ascii','ignore')).split(". ")))
 	for i in section_names:
 		temp = response.section(i)
-		sections_content.append((i, (str(temp.encode('ascii','ignore')).split("."))))
+		try:
+			sections_content.append((i, (str(temp.encode('ascii','ignore')).split("."))))
+		except AttributeError:
+			continue
 
-#	for i in sections_content:
-#		print i[0] + ": "
-#		print " "
-#		for j in i[1]:
-#			print j
-#			
-#		print " "
+	for i in sections_content:
+		if i[0] == "See also":
+			break
+		print i[0] + ": "
+		print " "
+		for j in i[1]:
+			print j.strip()
+			
+		print " "
 
 
 	#s.encode('ascii','ignore')
@@ -44,11 +77,5 @@ def prep(query):
 	#print sentence_list
 
 if __name__=="__main__":
-	query = raw_input("What would you like to search?  ")
+	prep()
 
-prep(query)
-
-#for i in q_response.content:
-	#print i
-
-# replacement_json = requests.get(https://wordsapiv1.p.mashape.com/words/ /frequency
